c7381d7670509466d5e6cb8927a192679a2136a4
Peter Keane <pkeane@peter-keanes-macbook.local>
2011-02-10 20:33:34 -0600

    factions

--- spearman.py
+++ spearman.py
@@ -29,16 +29,32 @@ def swap(data):
                 table[i].append('')
     return table
 
-def get_factions(filename):
-    data = file2table(filename)
-    toppers = data[0]
+def get_coordination_measure(filename):
+    filedata = file2table(filename)
+    toppers = filedata[0]
     factions = {}
     for i in range(len(toppers)):
         if toppers[i] not in factions:
             factions[toppers[i]] = []
         factions[toppers[i]].append(i)
-    print(max([x for x in (len(factions[n]))]))
-    print("FACTIONS: ", factions)
+    votes_per_tie = max([len(x) for x in factions.values()])
+    # get only factions that are in fact a set of tied ballots
+    sets = [fac for fac in factions.values() if len(fac) == votes_per_tie]
+    # print("FACTIONS: ", sets)
+    rhos = []
+    # create a faction data set
+    # make each ballot a row (instead of a column)
+    swapped_data = swap(filedata)
+    for set in sets:
+        data = []
+        for j in set:
+            data.append(swapped_data[j])
+        # data is now a faction of votes 
+        # get rhos w/in this faction
+        for pair in (list(itertools.combinations(list(range(len(data))),2))):
+            rhos.append(get_rho(data[pair[0]],data[pair[1]]))
+    return get_avg(rhos)
+    #print(file,'{0:f}'.format(get_avg(rhos)))
 
 def get_rho(list1,list2):
     cands = sorted(list(set(list1+list2)))
@@ -71,18 +87,15 @@ if __name__ == '__main__':
         for name in files:
             file_count += 1
     print(str(file_count)+' TOTAL files to process')
+    exit()
     start_time = time.time()
     processed_files = 0
-    #BASEDIR = 'test'
+    # BASEDIR = 'test'
     for i in range(100):
         BASEDIR = 'ballots/'+str(i+1)
         for file in os.listdir(BASEDIR):
-            get_factions(BASEDIR+'/'+file)
-            data = file2ballots(BASEDIR+'/'+file)
-            rhos = []
-            for pair in (list(itertools.combinations(list(range(len(data))),2))):
-                rhos.append(get_rho(data[pair[0]],data[pair[1]]))
-            print(file,'{0:f}'.format(get_avg(rhos)))
+            cm = get_coordination_measure(BASEDIR+'/'+file)
+            print(file,' : ','{0:f}'.format(cm))
 
             now = time.time()
             elapsed_time = now - start_time
