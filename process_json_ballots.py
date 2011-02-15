3fa37ec3c8ac1a2d1d8e3b8b3ac66c5934099ca0
Peter Keane <pkeane@mail.utexas.edu>
2011-02-15 01:31:35 -0600

    hist

--- process_json_ballots.py
+++ process_json_ballots.py
@@ -2,13 +2,9 @@ import simplejson as json
 import os
 
 def fix_names(data):
-    cands = []
-    for row in data:
-        for cell in row:
-            cands.append(cell)
     i = 0
     new_name = {}
-    for c in sorted(list(set(cands))):
+    for c in get_cands(data):
         i += 1
         new_name[c] = 'c'+str(i)
     table = []
@@ -19,6 +15,25 @@ def fix_names(data):
         table.append(new_row)
     return table
 
+def get_cands(data):
+    cands = []
+    for row in data:
+        for cell in row:
+            cands.append(cell)
+    return sorted(list(set(cands)))
+
+def even_ballot_lengths(ballotrows):
+    ballot_length = len(get_cands(ballotrows))
+    for ballot in ballotrows:
+        diff = ballot_length - len(ballot)
+        ballot.extend(['-' for i in range(diff)])
+    return ballotrows
+
+def swap(data):
+    table = []
+    for i in range(len(data[0])):
+        table.append([row[i] for row in data])
+    return table
 
         
 
@@ -29,4 +44,5 @@ if __name__ == "__main__":
         fh = open(BASEDIR+'/'+file)
         data = json.loads(fh.read())
         print data['ELECTION']['id']
-        print fix_names(data['BALLOTS'])
+        # print fix_names(data['BALLOTS'])
+        print swap(even_ballot_lengths(data['BALLOTS']))
