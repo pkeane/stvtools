cd760c2daad34aefd01ca7a9d5ac497b4c0658fc
Peter Keane <pkeane@mail.utexas.edu>
2011-02-16 00:56:40 -0600

    tweaks

--- process_json_ballots.py
+++ process_json_ballots.py
@@ -1,5 +1,7 @@
+import copy
 import simplejson as json
 import os
+import sys
 
 def fix_names(data):
     i = 0
@@ -39,6 +41,28 @@ def check_ballots(data):
             if check_row[cell] > 1:
                 print "PROBLEM: "+cell
 
+def tweak(ballotrows,x,y):
+    # note x & y are 1 based !!
+    # and MUST be >= 1
+    print x,y
+    ballot = ballotrows[y-1]
+    if len(ballot) > x and ballot[x-1] != '-' and ballot[x] != '-':
+        first = ballot[x-1]
+        second = ballot[x]
+        ballotrows[y-1][x] = first 
+        ballotrows[y-1][x-1] = second 
+    return ballotrows
+
+def diff(ballotrows1,ballotrows2):
+    differences = '' 
+    for i in range(len(ballotrows1)):
+        for j in range(len(ballotrows1[i])):
+            if ballotrows1[i][j] != ballotrows2[i][j]:
+                differences += 'ballot {0} place {1} is different'.format(i,j)+"\n"
+    return differences
+
+
+
 def swap(data):
     table = []
     for i in range(len(data[0])):
@@ -46,12 +70,26 @@ def swap(data):
     return table
 
 if __name__ == "__main__":
+    TARGET = 'tweaked'
     BASEDIR = 'elections'
     for file in os.listdir(BASEDIR):
         fh = open(BASEDIR+'/'+file)
         data = json.loads(fh.read())
-        print data['ELECTION']['id']
+        year = data['ELECTION']['id']
         # print fix_names(data['BALLOTS'])
-        #print file
-        #print check_ballots(data['BALLOTS'])
-        #print swap(even_ballot_lengths(fix_names(data['BALLOTS'])))
+        # print file
+        # print check_ballots(data['BALLOTS'])
+        ballotrows = even_ballot_lengths(fix_names(data['BALLOTS']))
+        filename = "{0}-0-0.json".format(year)
+        with open(TARGET+'/'+filename, mode='w') as f:
+            json.dump(ballotrows, f)  
+        num_y = len(ballotrows)
+        num_x = len(ballotrows[0])
+        for y in range(num_y):
+            for x in range(num_x):
+                tweaked = tweak(copy.deepcopy(ballotrows),x+1,y+1)
+                # print diff(ballotrows,tweaked)
+                filename = "{0}-{1}-{2}.json".format(year,x+1,y+1)
+                with open(TARGET+'/'+filename, mode='w') as f:
+                    json.dump(tweaked, f)  
+                    print "printed {0}".format(filename)
